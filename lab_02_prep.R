99ff6adbd49d88c6b497a23c3352f3d7a84af298
kgfitzgerald <kgfitzgerald@u.northwestern.edu>
2024-09-06 17:25:01 -0700

    lab 01 commit

---
+++ lab_02_prep.R
@@ -0,0 +1,97 @@
+#https://la.myneighborhooddata.org/2021/06/poverty/
+
+library(tidyverse)
+library(janitor)
+
+grocery_1519 <- read_csv("data/All Years Grocery Store Access Calculations.csv") %>% 
+  clean_names()
+frpl_1523 <- read_csv("data/All Years FRPL Calculations.csv") %>% 
+  clean_names()
+poverty_1022 <- read_csv("data/All Years Poverty Calculations.csv") %>% 
+  clean_names()
+total_pop_1022 <- read_csv("data/All Years Total Population Calculations.csv") %>% 
+  clean_names()
+
+count(grocery_1519, year) #2015 & 2019 only
+count(frpl_1523, year) #2015 - 2023 all
+count(poverty_1022, year) #2010 - 2022 all
+
+
+length(unique(grocery_1519$geoid))
+length(unique(frpl_1523$geoid))
+
+food_desert_19 <- grocery_1519 %>% 
+  filter(year == 2019) %>% 
+  mutate(pct_lowaccess = lowaccess_count/denom_total_pop,
+         food_desert = case_when(pct_lowaccess >= 1/3 ~ "yes",
+                                 #lowaccess_count >= 500 ~ "yes",
+                                 TRUE ~ "no")) %>% 
+  select(geoid, geoid20, food_desert)
+
+data <- frpl_1523 %>% 
+  left_join(food_desert_19) %>% 
+  left_join(poverty_1022) %>% 
+  left_join(total_pop_1022) %>% 
+  filter(year > 2014, year < 2023)
+
+library(sf)
+# Load the shapefiles
+census_tracts <- st_read("data/tl_2019_06_tract/tl_2019_06_tract.shp")
+county_subdivisions <- st_read("data/tl_2019_06_cousub/tl_2019_06_cousub.shp")
+cong_dist <- st_read("data/tl_2023_06_cd118/tl_2023_06_cd118.shp")
+
+# Perform spatial join
+linked_data <- st_join(census_tracts, cong_dist, 
+                       join = st_intersects)
+
+# Extract relevant columns (e.g., geoid and county subdivision)
+result <- linked_data %>%
+  filter(COUNTYFP == "037") %>% 
+  select(census_tract_geoid = GEOID.x, cong_dist = NAMELSAD.y) %>% 
+  st_drop_geometry() %>% 
+  mutate(census_tract_geoid = as.numeric(census_tract_geoid)) %>% 
+  distinct()
+
+
+cong_dist <- data %>% 
+  left_join(result, by = c("geoid20" = "census_tract_geoid")) %>% 
+  distinct(geoid20, year, .keep_all = TRUE) %>% 
+  group_by(cong_dist, year) %>% 
+  mutate(n_tracts = n()) %>% 
+  filter(n_tracts >= 10) %>% 
+  ungroup() %>% 
+  select(-n_tracts)
+
+write_csv(data, "./data/la_county_food_insecurity.csv")
+
+data_22 <- data %>% 
+  filter(year == 2022)
+
+summary(data_22$frpl_pct)
+summary(data_22$reducedmeals_count)
+summary(data_22$freemeals_count)
+
+sum(data_22$denom_known_poverty)
+
+data %>% 
+  group_by(year) %>% 
+  summarize(n_total_pop = sum(total_pop, na.rm = TRUE),
+            n_denom_known = sum(denom_known_poverty, na.rm = TRUE),
+            n_below_poverty = sum(pop_below_100_count, na.rm = TRUE),
+            med_pop_below_100_pct = median(pop_below_100_pct, na.rm = TRUE))
+
+cong_dist %>% 
+  filter(year == 2019) %>% 
+  group_by(cong_dist) %>% 
+  summarize(n = n(),
+            min = min(frpl_pct, na.rm = TRUE),
+            Q1 = quantile(frpl_pct, .25, na.rm = TRUE),
+            Q2 = quantile(frpl_pct, .5, na.rm = TRUE),
+            median = median(frpl_pct, .5, na.rm = TRUE),
+            Q3 = quantile(frpl_pct, .75, na.rm = TRUE),
+            max = max(frpl_pct, na.rm = TRUE)
+  ) %>% 
+  arrange(min)
+
+
+            
